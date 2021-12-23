b1347bf015c18937c3c5473e6565bf747e71fbf0
Vasu Sheoran <gokusayon@gmail.com>
2021-12-23 18:22:16 +0530

    Fixed mount volume for symbols. Fixed start.bat script.

--- start.bat
+++ start.bat
@@ -3,7 +3,13 @@
 
 echo "Starting ..."
 
+set "pwd=%cd%"
+
 cd golang\client-app\http && go build -o trends-client-app.exe main.go
+
+echo %pwd%
+
+cd %pwd%
 
 docker-compose -f docker-compose.yml down
 
