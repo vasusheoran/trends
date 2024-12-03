ee095900a3899836b980679a8536e7a0b92516a0
Vasu Sheoran <vsheoran@salesforce.com>
2024-12-04 03:22:25 +0530

    updated history templ

--- templates/history/get_templ.go
+++ templates/history/get_templ.go
@@ -34,7 +34,7 @@ func GetHistory(stocks []contracts.Stock, ticker string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div id=\"history-section\"><span class=\"material-symbols-outlined cursor-pointer\" hx-get=\"/history/close\" hx-trigger=\"click\" hx-swap=\"outerHTML\" hx-target=\"#history-section\">close</span><h2>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div id=\"history-section\"><span class=\"material-symbols-outlined cursor-pointer\" hx-get=\"/select/close\" hx-trigger=\"click\" hx-swap=\"outerHTML\" hx-target=\"#history-section\">close</span><h2>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
