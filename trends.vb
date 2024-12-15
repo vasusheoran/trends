122ee6aee1ad927572d2888c401e02627e8cafb9
Vasu Sheoran <gokusayon@gmail.com>
2024-12-16 00:38:27 +0530

    updated vb file

--- trends.vb
+++ trends.vb
@@ -1,3 +1,18 @@
+Sub Test()
+
+    Dim result As String
+    
+    result = PostTrend("NF", _
+                    "2/12/24", _
+                    2, _
+                    3, _
+                    4, _
+                    5)
+    Debug.Print result
+    
+End Sub
+
+
 ' If a function's arguments are defined as follows:
 Function PostTrend(ticker As String, _
                 d As String, _
@@ -8,30 +23,25 @@ Function PostTrend(ticker As String, _
                 Optional URL = "http://localhost:5001/api/update/index")
                 
 Dim data As String
-data = "{""date"":" & d & _
-        ",""ticker"":""" & ticker & _
+data = "{""date"":""" & d & _
+        """,""ticker"":""" & ticker & _
         """,""open"":" & o & _
-        ",""close""," & c & _
+        ",""close"":" & c & _
         ",""high"":" & h & _
         ",""low"":" & l & " }"
+        
+Debug.Print data
         
 Dim hReq As Object
 Set hReq = CreateObject("MSXML2.XMLHTTP")
     With hReq
         .Open "PUT", URL, False
         .SetRequestHeader "Content-Type", "application/json"
-        .SetTimeouts 2000, 2000, 2000, 2000
-        .Send d
+        '.SetTimeouts 2000, 2000, 2000, 2000
+        .Send data
     End With
-
-'.OnTimeOut = OnTimeOutMessage
-Debug.Print hReq.ResponseText
-Debug.Print URL
-Debug.Print data
-
+    
 PostTrend = hReq.ResponseText
-
-'PostTrend = Trend(URL, data)
 End Function
 
 
